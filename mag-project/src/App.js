edeb7dc285d8054126e0487d8dabce7fe46611ab
Gelo Torres <michael1981torres@gmail.com>
2022-03-08 23:44:18 +0800

    updated pages folder

--- mag-project/src/App.js
+++ mag-project/src/App.js
@@ -13,12 +13,28 @@ import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 import Sample from './components/Sample'
 import SampleNav from './components/SampleNav';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+import Testimonials from './pages/Testimonials';
+
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <SampleNav />
+        <Router>
+      <Routes>
+        <Route exact path = "/" element = {<Home/>}></Route>
+        <Route exact path = "/About" element = {<About/>}></Route>
+        <Route exact path = "Contact" element = {<Contact/>}></Route>
+        <Route exact path = "Services" element = {<Services/>}></Route>
+        <Route exact path = "Testimonials" element = {<Testimonials/>}></Route>
+      </Routes>
+   </Router>
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
