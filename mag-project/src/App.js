35b0553f04a2ace390622f7a8b704246012152ec
Marcus Rejinal Garcia <marcusrejinalgarcia@gmail.com>
2022-03-09 00:21:24 +0800

    fixed alignment of code

--- mag-project/src/App.js
+++ mag-project/src/App.js
@@ -11,16 +11,16 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
-import Sample from './components/Sample'
+import Sample from './components/Sample';
 import SampleNav from './components/SampleNav';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Services from './pages/Services';
 import Testimonials from './pages/Testimonials';
 
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -29,21 +29,16 @@ function App() {
         <Navbar />
         <SampleNav />
         <Router>
-      <Routes>
-        <Route exact path = "/" element = {<Home/>}></Route>
-        <Route exact path = "/About" element = {<About/>}></Route>
-        <Route exact path = "Contact" element = {<Contact/>}></Route>
-        <Route exact path = "Services" element = {<Services/>}></Route>
-        <Route exact path = "Testimonials" element = {<Testimonials/>}></Route>
-      </Routes>
-   </Router>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/About" element={<About />}></Route>
+            <Route exact path="Contact" element={<Contact />}></Route>
+            <Route exact path="Services" element={<Services />}></Route>
+            <Route exact path="Testimonials" element={<Testimonials />}></Route>
+          </Routes>
+        </Router>
         <Grid minH="100vh" p={3}>
-          
-          <VStack spacing={8}>
-            
-            
-            
-          </VStack>
+          <VStack spacing={8}></VStack>
         </Grid>
       </Box>
     </ChakraProvider>
