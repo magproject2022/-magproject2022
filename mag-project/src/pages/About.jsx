ed5b8071b4b9bddc659d58f269a12e914fec8cc4
Marcus Rejinal Garcia <marcusrejinalgarcia@gmail.com>
2022-03-10 01:50:55 +0800

    added testimony cards and edited about page

--- mag-project/src/pages/About.jsx
+++ mag-project/src/pages/About.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Box, Image, Heading, Text, Button, Container } from '@chakra-ui/react'
 import ImageHeader from '../components/ImageHeader'
 
 const About = () => {
   return (
     <>
       <ImageHeader />
+      <Box minHeight='calc(100vh - 584px)' display='flex' flexDir='column' justifyContent='center' alignItems='center' gap='2em'>
+        <Container>
+          We are a team of three who, despite our differences, found ourselves in a Web Development Bootcamp by KodeGo. Among other things, we mainly share one thing in common: a passion for coding.
+        </Container>
+        <Button>Learn more</Button>
+      </Box>
     </>
   )
 }
